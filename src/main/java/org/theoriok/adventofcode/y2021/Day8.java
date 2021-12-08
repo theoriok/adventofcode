f50398aadbdc4091cd752b71de8331ce57d500ed
Kevin Van Dyck <kevin.vandyck@conundra.eu>
2021-12-08 07:13:34 +0100

    setup day 8

---
+++ src/main/java/org/theoriok/adventofcode/y2021/Day8.java
@@ -0,0 +1,14 @@
+package org.theoriok.adventofcode.y2021;
+
+import org.theoriok.adventofcode.Day;
+
+import java.util.List;
+
+public class Day8 extends Day {
+
+    public Day8(List<String> input) {
+        super(input);
+    }
+
+
+}
---
+++ src/test/java/org/theoriok/adventofcode/y2021/Day8Test.java
@@ -0,0 +1,41 @@
+package org.theoriok.adventofcode.y2021;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Day8Test {
+
+    private static final List<String> LIST = List.of(
+        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+        "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+        "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+        "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+        "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+        "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+        "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+        "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+        "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+        "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+    );
+
+    @Test
+    void firstMethod() {
+        var day = new Day8(LIST);
+
+        var result = day.firstMethod();
+
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    void secondMethod() {
+        var day = new Day8(LIST);
+
+        var result = day.secondMethod();
+
+        assertThat(result).isEqualTo(-1);
+    }
+}
