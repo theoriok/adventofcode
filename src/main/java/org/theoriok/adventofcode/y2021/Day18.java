df4d6de9ac38f4c143e0d4c890b30d99608d3b79
Kevin <kevin.van.dyck@gmail.com>
2021-12-19 21:11:39 +0100

    day 18 wip

--- src/main/java/org/theoriok/adventofcode/y2021/Day18.java
+++ src/main/java/org/theoriok/adventofcode/y2021/Day18.java
@@ -4,14 +4,39 @@ import org.theoriok.adventofcode.Day;
 
 import java.util.List;
 
-public class Day18 extends Day<String, Integer> {
+public class Day18 extends Day<Integer, Integer> {
 
     public Day18(List<String> input) {
         super(input);
+        var pairs 
     }
 
     @Override
-    public String firstMethod() {
-        return "";
+    public Integer firstMethod() {
+        return 0;
+    }
+
+    private static class Pair<L, R> {
+        private final L left;
+        private final R right;
+
+        private Pair(L left, R right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        public long magnitude() {
+            return magnitude(left) * 3 + magnitude(right) * 2;
+        }
+
+        private long magnitude(Object value) {
+            if (value instanceof Integer) {
+                return (Integer) value;
+            } else if (value instanceof Pair) {
+                return ((Pair<?, ?>) value).magnitude();
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
--- src/test/java/org/theoriok/adventofcode/y2021/Day18Test.java
+++ src/test/java/org/theoriok/adventofcode/y2021/Day18Test.java
@@ -2,17 +2,52 @@ package org.theoriok.adventofcode.y2021;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Day18Test {
 
+    private static final List<String> LIST1 = List.of(
+        "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+        "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+        "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+        "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+        "[7,[5,[[3,8],[1,4]]]]",
+        "[[2,[2,2]],[8,[8,1]]]",
+        "[2,9]",
+        "[1,[[[9,3],9],[[9,0],[0,7]]]]",
+        "[[[5,[7,4]],7],1]",
+        "[[[[4,2],2],6],[8,7]]"
+    );
+
+    private static final List<String> LIST2 = List.of(
+        "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+        "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+        "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+        "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+        "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+        "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+        "[[[[5,4],[7,7]],8],[[8,3],8]]",
+        "[[9,3],[[9,9],[6,[4,9]]]]",
+        "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+        "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
+    );
+
+    public static Stream<Arguments> firstMethodSource() {
+        return Stream.of(
+            Arguments.of(LIST1, 3488),
+            Arguments.of(LIST2, 4140)
+        );
+    }
+
     @ParameterizedTest
-    @Disabled
+    @MethodSource("firstMethodSource")
     void firstMethod(List<String> list, Long expectedResult) {
-        var day = new Day16(list);
+        var day = new Day18(list);
 
         var result = day.firstMethod();
 
