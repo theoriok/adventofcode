989bb33ffb081a1a047f2234dc54f897912cc26c
Kevin Van Dyck <kevin.vandyck@conundra.eu>
2022-08-16 08:58:57 +0200

    day 3 part 1

--- src/main/java/org/theoriok/adventofcode/y2020/Day3.java
+++ src/main/java/org/theoriok/adventofcode/y2020/Day3.java
@@ -4,14 +4,39 @@ import org.theoriok.adventofcode.Day;
 
 import java.util.List;
 
-public class Day3 extends Day<Long, Long> {
+public class Day3 extends Day<Integer, Long> {
+
+    private int height;
+    private int width;
+    private char[][] grid;
 
     public Day3(List<String> input) {
+        initializeGrid(input);
+    }
 
+    private void initializeGrid(List<String> input) {
+        width = input.get(0).length();
+        height = input.size();
+        grid = new char[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                grid[x][y] = input.get(y).charAt(x);
+            }
+        }
     }
 
     @Override
-    public Long firstMethod() {
-        return super.firstMethod();
+    public Integer firstMethod() {
+        var x = 0;
+        var y = 0;
+        var count = 0;
+        while (y < height) {
+            if (grid[x % width][y] == '#') {
+                count++;
+            }
+            x += 3;
+            y += 1;
+        }
+        return count;
     }
 }
