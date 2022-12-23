3d6730f144568817a99facca1c3e1ef7c227270a
Kevin <kevin.van.dyck@gmail.com>
2022-12-23 18:18:44 +0100

    checkstyle

--- src/main/java/org/theoriok/adventofcode/y2022/Day3.java
+++ src/main/java/org/theoriok/adventofcode/y2022/Day3.java
@@ -3,14 +3,12 @@ package org.theoriok.adventofcode.y2022;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import org.apache.commons.lang3.StringUtils;
 import org.theoriok.adventofcode.Day;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
 
 public class Day3 extends Day<Integer, Integer> {
 
