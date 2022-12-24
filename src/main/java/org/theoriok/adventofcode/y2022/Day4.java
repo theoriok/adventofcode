f722d6aaeb869c9cdb8b3c47974c27bd3ccc2f4b
Kevin <kevin.van.dyck@gmail.com>
2022-12-24 18:08:54 +0100

    day4 part 1

---
+++ src/main/java/org/theoriok/adventofcode/y2022/Day4.java
@@ -0,0 +1,49 @@
+package org.theoriok.adventofcode.y2022;
+
+import com.google.common.collect.Range;
+import org.theoriok.adventofcode.Day;
+
+import java.util.List;
+
+public class Day4 extends Day<Long, Integer> {
+
+    private final List<Assignments> assignments;
+
+    public Day4(List<String> input) {
+        assignments = input.stream()
+            .map(this::toAssignments)
+            .toList();
+    }
+
+    private Assignments toAssignments(String line) {
+        var parts = line.split(",");
+        return new Assignments(toAssignment(parts[0]), toAssignment(parts[1]));
+    }
+
+    private Assignment toAssignment(String part) {
+        var parts = part.split("-");
+        return new Assignment(Range.closed(Integer.parseInt(parts[0]), Integer.parseInt(parts[1])));
+    }
+
+    @Override
+    public Long firstMethod() {
+        return assignments.stream()
+            .filter(Assignments::fullyContained)
+            .count();
+    }
+
+    private record Assignment(Range<Integer> range) {
+        public boolean encloses(Assignment other) {
+            return range.encloses(other.range);
+        }
+    }
+
+    private record Assignments(
+        Assignment first,
+        Assignment second
+    ) {
+        boolean fullyContained() {
+            return first.encloses(second) || second.encloses(first);
+        }
+    }
+}
---
+++ src/test/java/org/theoriok/adventofcode/y2022/Day4Test.java
@@ -0,0 +1,29 @@
+package org.theoriok.adventofcode.y2022;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Day4Test {
+
+    private static final List<String> LIST = List.of(
+        "2-4,6-8",
+        "2-3,4-5",
+        "5-7,7-9",
+        "2-8,3-7",
+        "6-6,4-6",
+        "2-6,4-8"
+    );
+
+    @Test
+    void firstMethod() {
+        var day = new Day4(LIST);
+
+        var result = day.firstMethod();
+
+        assertThat(result).isEqualTo(2);
+    }
+
+}
