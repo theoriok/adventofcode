c0b992abcf4354b60c8da9d2be111a8a1cbcf3d5
Kevin <kevin.van.dyck@gmail.com>
2023-12-10 23:21:12 +0100

    day 4 part 1

---
+++ src/main/java/org/theoriok/adventofcode/y2023/Day4.java
@@ -0,0 +1,51 @@
+package org.theoriok.adventofcode.y2023;
+
+import org.apache.commons.collections4.ListUtils;
+import org.theoriok.adventofcode.Day;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Day4 implements Day<Long, Long> {
+
+    private final List<Card> cards;
+
+    public Day4(List<String> input) {
+        cards = input.stream()
+            .map(Card::fromString)
+            .toList();
+    }
+
+    private record Card(int index, List<Integer> winningNumbers, List<Integer> numbers) {
+        public static Card fromString(String input) {
+            String[] firstSplit = input.split(":");
+            var index = Integer.parseInt(firstSplit[0].replace("Card", "").trim());
+            String[] secondSplit = firstSplit[1].split("\\|");
+            var winningNumbers = splitOnSpaces(secondSplit[0]);
+            var numbers = splitOnSpaces(secondSplit[1]);
+            return new Card(index, winningNumbers, numbers);
+        }
+
+        private static List<Integer> splitOnSpaces(String input) {
+            return Arrays.stream(input.trim().split(" +")).map(part -> Integer.parseInt(part.trim())).toList();
+        }
+
+        public long countPoints() {
+            List<Integer> wonNumbers = ListUtils.intersection(winningNumbers, numbers);
+            return wonNumbers.size() > 0 ? (long) (Math.pow(2, wonNumbers.size() - 1)) : 0L;
+        }
+
+    }
+
+    @Override
+    public Long firstMethod() {
+        return cards.stream()
+            .mapToLong(card -> card.countPoints())
+            .sum();
+    }
+
+    @Override
+    public Long secondMethod() {
+        return 0L;
+    }
+}
---
+++ src/test/java/org/theoriok/adventofcode/y2023/Day4Test.java
@@ -0,0 +1,37 @@
+package org.theoriok.adventofcode.y2023;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Day4Test {
+
+    private static final List<String> LIST = List.of(
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+    );
+
+    @Test
+    void firstMethod() {
+        var day = new Day4(LIST);
+
+        var result = day.firstMethod();
+
+        assertThat(result).isEqualTo(13);
+    }
+
+    @Test
+    void secondMethod() {
+        var day = new Day4(LIST);
+
+        var result = day.secondMethod();
+
+        assertThat(result).isEqualTo(0);
+    }
+}
