1064179ab24a8b42e9d008274ba370e3146bdc8f
Kevin <kevin.van.dyck@gmail.com>
2023-12-10 13:49:56 +0100

    2023 day one

--- src/test/java/org/theoriok/adventofcode/y2023/Day1Test.java
+++ src/test/java/org/theoriok/adventofcode/y2023/Day1Test.java
@@ -9,20 +9,20 @@ import java.util.List;
 class Day1Test {
 
     private static final List<String> LIST1 = List.of(
-        "1abc2",
-        "pqr3stu8vwx",
-        "a1b2c3d4e5f",
-        "treb7uchet"
+            "1abc2",
+            "pqr3stu8vwx",
+            "a1b2c3d4e5f",
+            "treb7uchet"
     );
 
     private static final List<String> LIST2 = List.of(
-        "two1nine",
-        "eightwothree",
-        "abcone2threexyz",
-        "xtwone3four",
-        "4nineeightseven2",
-        "zoneight234",
-        "7pqrstsixteen"
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen"
     );
 
     @Test
@@ -41,6 +41,15 @@ class Day1Test {
         var result = day.secondMethod();
 
         assertThat(result).isEqualTo(281);
+    }
+
+    @Test
+    void secondMethod_overlappingStringNumbers() {
+        var day = new Day1(List.of("seven148oneightd"));
+
+        var result = day.secondMethod();
+
+        assertThat(result).isEqualTo(78);
     }
 
 }
