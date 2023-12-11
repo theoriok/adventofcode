0d2e6bc5cd346f82a3c2b9ec05a32615a2fb7c38
Kevin <kevin.van.dyck@gmail.com>
2023-12-11 23:16:39 +0100

    day5 start

---
+++ src/main/java/org/theoriok/adventofcode/y2023/Day5.java
@@ -0,0 +1,22 @@
+package org.theoriok.adventofcode.y2023;
+
+import org.theoriok.adventofcode.Day;
+
+import java.util.List;
+
+public class Day5 implements Day<Long, Long> {
+
+    public Day5(List<String> input) {
+
+    }
+
+    @Override
+    public Long firstMethod() {
+        return 0L;
+    }
+
+    @Override
+    public Long secondMethod() {
+        return 0L;
+    }
+}
---
+++ src/test/java/org/theoriok/adventofcode/y2023/Day5Test.java
@@ -0,0 +1,64 @@
+package org.theoriok.adventofcode.y2023;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Day5Test {
+
+    private static final List<String> LIST = List.of(
+        "seeds: 79 14 55 13",
+        "",
+        "seed-to-soil map:",
+        "50 98 2",
+        "52 50 48",
+        "",
+        "soil-to-fertilizer map:",
+        "0 15 37",
+        "37 52 2",
+        "39 0 15",
+        "",
+        "fertilizer-to-water map:",
+        "49 53 8",
+        "0 11 42",
+        "42 0 7",
+        "57 7 4",
+        "",
+        "water-to-light map:",
+        "88 18 7",
+        "18 25 70",
+        "",
+        "light-to-temperature map:",
+        "45 77 23",
+        "81 45 19",
+        "68 64 13",
+        "",
+        "temperature-to-humidity map:",
+        "0 69 1",
+        "1 0 69",
+        "",
+        "humidity-to-location map:",
+        "60 56 37",
+        "56 93 4"
+    );
+
+    @Test
+    void firstMethod() {
+        var day = new Day4(LIST);
+
+        var result = day.firstMethod();
+
+        assertThat(result).isEqualTo(35);
+    }
+
+    @Test
+    void secondMethod() {
+        var day = new Day4(LIST);
+
+        var result = day.secondMethod();
+
+        assertThat(result).isEqualTo(0);
+    }
+}
