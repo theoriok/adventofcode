3681600a319074b129e42000773491ccff5a0ec3
Kevin Van Dyck <kevin.vandyck@conundra.eu>
2023-12-12 20:37:18 +0100

    day 5 wip

--- src/main/java/org/theoriok/adventofcode/y2023/Day5.java
+++ src/main/java/org/theoriok/adventofcode/y2023/Day5.java
@@ -2,12 +2,13 @@ package org.theoriok.adventofcode.y2023;
 
 import org.theoriok.adventofcode.Day;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Day5 implements Day<Long, Long> {
 
     public Day5(List<String> input) {
-
+        var seeds = Arrays.stream(input.getFirst().replace("seeds: ", "").split(" ")).map(Long::parseLong).toList();
     }
 
     @Override
