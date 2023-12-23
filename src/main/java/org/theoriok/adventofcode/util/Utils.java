e0f24cc4328ac32f2d702844c6a47ee689bbcae4
Kevin <kevin.van.dyck@gmail.com>
2023-12-23 20:42:09 +0100

    add private constructor

--- src/main/java/org/theoriok/adventofcode/util/Utils.java
+++ src/main/java/org/theoriok/adventofcode/util/Utils.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.function.Function;
 
 public class Utils {
+    private Utils() {
+    }
 
     public static <T> List<T> splitToList(String input, String regex, Function<String, T> mapper) {
         return Arrays.stream(input.split(regex))
