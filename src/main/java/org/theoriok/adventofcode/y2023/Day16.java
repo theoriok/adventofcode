61f12bc6d54e36fc92848e2f3f4e5d9182f26502
Kevin <kevin.van.dyck@gmail.com>
2024-01-09 20:31:10 +0100

    day 16 setup

---
+++ src/main/java/org/theoriok/adventofcode/y2023/Day16.java
@@ -0,0 +1,25 @@
+package org.theoriok.adventofcode.y2023;
+
+import org.theoriok.adventofcode.Day;
+
+import java.util.List;
+
+public class Day16 implements Day<Long, Long> {
+
+    private final List<String> input;
+
+    public Day16(List<String> input) {
+
+        this.input = input;
+    }
+
+    @Override
+    public Long firstMethod() {
+        return 0L;
+    }
+
+    @Override
+    public Long secondMethod() {
+        return 0L;
+    }
+}
