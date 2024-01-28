de97f42d7b92e6b088df1b18c498296d37a0f95c
Kevin <kevin.van.dyck@gmail.com>
2024-01-28 15:43:59 +0100

    day 17 part 1 test

--- src/test/java/org/theoriok/adventofcode/y2023/Day17Test.java
+++ src/test/java/org/theoriok/adventofcode/y2023/Day17Test.java
@@ -9,7 +9,19 @@ import java.util.List;
 class Day17Test {
 
     private static final List<String> LIST = List.of(
-
+        "2413432311323",
+        "3215453535623",
+        "3255245654254",
+        "3446585845452",
+        "4546657867536",
+        "1438598798454",
+        "4457876987766",
+        "3637877979653",
+        "4654967986887",
+        "4564679986453",
+        "1224686865563",
+        "2546548887735",
+        "4322674655533"
     );
 
     @Test
@@ -18,7 +30,7 @@ class Day17Test {
 
         var result = day.firstMethod();
 
-        assertThat(result).isEqualTo(0);
+        assertThat(result).isEqualTo(102);
     }
 
     @Test
