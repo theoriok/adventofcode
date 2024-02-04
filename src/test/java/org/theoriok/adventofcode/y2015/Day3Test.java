99828831e4f3c3020c1fdc81ff0238df9ad2183a
Kevin <kevin.van.dyck@gmail.com>
2024-02-04 13:37:11 +0100

    2015 day 3

---
+++ src/test/java/org/theoriok/adventofcode/y2015/Day3Test.java
@@ -0,0 +1,39 @@
+package org.theoriok.adventofcode.y2015;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+class Day3Test {
+
+    @ParameterizedTest
+    @CsvSource({
+        ">,2",
+        "^>v<,4",
+        "^v^v^v^v^v,2",
+    })
+    void firstMethod(String input, int expected) {
+        var day = new Day3(List.of(input));
+
+        var result = day.firstMethod();
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "^v,3",
+        "^>v<,3",
+        "^v^v^v^v^v,11",
+    })
+    void secondMethod(String input, int expected) {
+        var day = new Day3(List.of(input));
+
+        var result = day.secondMethod();
+
+        assertThat(result).isEqualTo(expected);
+    }
+}
