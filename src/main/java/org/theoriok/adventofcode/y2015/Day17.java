6ae7ad38b31602969db32c4bb4c2ffbd6d0e1543
Kevin Van Dyck <kevin.van.dyck@gmail.com>
2024-11-03 17:23:45 +0100

    2015 day 17

---
+++ src/main/java/org/theoriok/adventofcode/y2015/Day17.java
@@ -0,0 +1,71 @@
+package org.theoriok.adventofcode.y2015;
+
+import com.google.common.collect.Sets;
+import org.theoriok.adventofcode.Day;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.groupingBy;
+
+public class Day17 implements Day<Long, Integer> {
+
+    private static final int EGGNOG_VOLUME = 150;
+    private final List<Jar> jars;
+
+    public Day17(List<String> input) {
+        jars = input.stream()
+            .map(Jar::from)
+            .toList();
+    }
+
+    @Override
+    public Long firstMethod() {
+        return firstMethod(EGGNOG_VOLUME);
+    }
+
+    public Long firstMethod(int volume) {
+        return allSubsets(jars).stream()
+            .filter(subset -> fitsVolumeExactly(subset, volume))
+            .count();
+    }
+
+    private boolean fitsVolumeExactly(Collection<Jar> subset, int volume) {
+        return subset.stream()
+            .mapToInt(Jar::volume)
+            .sum() == volume;
+    }
+
+    private Collection<? extends Collection<Jar>> allSubsets(List<Jar> jars) {
+        return IntStream.range(1, jars.size())
+            .boxed()
+            .flatMap(size -> Sets.combinations(new HashSet<>(jars), size).stream())
+            .toList();
+    }
+
+    @Override
+    public Integer secondMethod() {
+        return secondMethod(EGGNOG_VOLUME);
+    }
+
+    public int secondMethod(int volume) {
+        return allSubsets(jars).stream()
+            .filter(subset -> fitsVolumeExactly(subset, volume))
+            .collect(groupingBy(Collection::size))
+            .entrySet().stream()
+            .min(Map.Entry.comparingByKey())
+            .orElseThrow()
+            .getValue()
+            .size();
+    }
+
+    record Jar(int volume, UUID id) {
+        public static Jar from(String line) {
+            return new Jar(Integer.parseInt(line), UUID.randomUUID());
+        }
+    }
+}
---
+++ src/test/java/org/theoriok/adventofcode/y2015/Day17Test.java
@@ -0,0 +1,31 @@
+package org.theoriok.adventofcode.y2015;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Day17Test {
+
+    private static final List<String> LIST = List.of("20", "15", "10", "5", "5");
+    private static final int TEST_VOLUME = 25;
+
+    @Test
+    void firstMethod() {
+        var day = new Day17(LIST);
+
+        var result = day.firstMethod(TEST_VOLUME);
+
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test
+    void secondMethod() {
+        var day = new Day17(LIST);
+
+        var result = day.secondMethod(TEST_VOLUME);
+
+        assertThat(result).isEqualTo(3);
+    }
+}
