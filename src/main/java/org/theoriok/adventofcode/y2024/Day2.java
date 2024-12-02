a8dd7aacca2542c74f9435017d8aa1bb00880d81
theoriok <kevin.van.dyck@gmail.com>
2024-12-02 11:04:36 +0100

    2024 day 2

---
+++ src/main/java/org/theoriok/adventofcode/y2024/Day2.java
@@ -0,0 +1,73 @@
+package org.theoriok.adventofcode.y2024;
+
+import static org.theoriok.adventofcode.util.Utils.splitToList;
+
+import com.google.common.collect.Collections2;
+import org.theoriok.adventofcode.Day;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day2 implements Day<Long, Long> {
+
+    private final List<Report> reports;
+
+    public Day2(List<String> input) {
+        reports = input.stream()
+            .map(line -> splitToList(line, " ", Integer::parseInt))
+            .map(Report::new)
+            .toList();
+    }
+
+    @Override
+    public Long firstMethod() {
+        return reports.stream()
+            .filter(Report::safe)
+            .count();
+    }
+
+    @Override
+    public Long secondMethod() {
+        return reports.stream()
+            .filter(Report::safeWithDampening)
+            .count();
+    }
+
+    record Report(List<Integer> levels) {
+        public boolean safe() {
+            return safe(levels);
+        }
+
+        private boolean safe(List<Integer> levels) {
+            List<Integer> increases = new ArrayList<>();
+            for (int i = 1; i < levels.size(); i++) {
+                increases.add(levels.get(i) - levels.get(i - 1));
+            }
+            var isPositive = increases.getFirst() > 0;
+            for (Integer increase : increases) {
+                if (increase > 0 != isPositive) {
+                    return false;
+                }
+                if (1 > Math.abs(increase) || Math.abs(increase) > 3) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public boolean safeWithDampening() {
+            for (int i = 0; i < levels.size(); i++) {
+               List<Integer> levelsWithOneRemoved = new ArrayList<>();
+                for (int j = 0; j < levels.size(); j++) {
+                    if (i != j) {
+                        levelsWithOneRemoved.add(levels.get(j));
+                    }
+                }
+                if (safe(levelsWithOneRemoved)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ src/test/java/org/theoriok/adventofcode/y2024/Day2Test.java
@@ -0,0 +1,38 @@
+package org.theoriok.adventofcode.y2024;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.theoriok.adventofcode.GenericDay;
+
+import java.util.List;
+
+class Day2Test {
+
+    private static final List<String> LIST = List.of(
+        "7 6 4 2 1",
+        "1 2 7 8 9",
+        "9 7 6 2 1",
+        "1 3 2 4 5",
+        "8 6 4 4 1",
+        "1 3 6 7 9"
+    );
+
+    @Test
+    void firstMethod() {
+        var day = new Day2(LIST);
+
+        var result = day.firstMethod();
+
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void secondMethod() {
+        var day = new Day2(LIST);
+
+        var result = day.secondMethod();
+
+        assertThat(result).isEqualTo(4);
+    }
+}
