9ad54d35f9e1cbedfea105b41ee087699a3a5478
Kevin Van Dyck <kevin.van.dyck@gmail.com>
2024-12-05 08:37:37 +0100

    2024 day 5

---
+++ src/main/java/org/theoriok/adventofcode/y2024/Day5.java
@@ -0,0 +1,91 @@
+package org.theoriok.adventofcode.y2024;
+
+import org.theoriok.adventofcode.Day;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.theoriok.adventofcode.util.Utils.splitToList;
+
+public class Day5 implements Day<Integer, Integer> {
+
+    private final List<Rule> rules;
+    private final List<PageList> pageLists;
+
+    public Day5(List<String> input) {
+        int emptyLine = input.indexOf("");
+        rules = input.subList(0, emptyLine).stream()
+            .map(Rule::from)
+            .toList();
+        pageLists = input.subList(emptyLine + 1, input.size()).stream()
+            .map(PageList::from)
+            .toList();
+    }
+
+    @Override
+    public Integer firstMethod() {
+        return pageLists.stream()
+            .filter(pageList -> pageList.isValid(rules))
+            .mapToInt(PageList::middlePage)
+            .sum();
+    }
+
+    @Override
+    public Integer secondMethod() {
+        return pageLists.stream()
+            .filter(pageList -> !pageList.isValid(rules))
+            .map(pageList -> pageList.reOrderedPages(rules))
+            .mapToInt(PageList::middlePage)
+            .sum();
+    }
+
+    record Rule(int firstPage, int secondPage) {
+        public static Rule from(String line) {
+            String[] split = line.split("\\|");
+            return new Rule(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+        }
+
+        public boolean validate(List<Integer> pages) {
+            return pages.indexOf(firstPage()) < pages.indexOf(secondPage());
+        }
+
+        public boolean appliesTo(List<Integer> pages) {
+            return pages.contains(firstPage()) && pages.contains(secondPage());
+        }
+    }
+
+    record PageList(List<Integer> pages) {
+        public static PageList from(String line) {
+            return new PageList(splitToList(line, ",", Integer::parseInt));
+        }
+
+        public boolean isValid(List<Rule> rules) {
+            return rules.stream()
+                .filter(rule -> rule.appliesTo(pages))
+                .allMatch(rule -> rule.validate(pages));
+        }
+
+        public int middlePage() {
+            return pages.get(pages.size() / 2);
+        }
+
+        public PageList reOrderedPages(List<Rule> rules) {
+            var reOrderedPages = new ArrayList<>(pages);
+            reOrderedPages.sort(compareAccordingToTheRules(rules));
+
+            return new PageList(reOrderedPages);
+        }
+
+        private Comparator<Integer> compareAccordingToTheRules(List<Rule> rules) {
+            return (page1, page2) -> {
+                List<Integer> pagesToSort = List.of(page1, page2);
+                return rules.stream()
+                    .filter(rule -> rule.appliesTo(pagesToSort))
+                    .findFirst()
+                    .map(rule -> rule.validate(pagesToSort) ? 1 : -1)
+                    .orElse(0);
+            };
+        }
+    }
+}
---
+++ src/test/java/org/theoriok/adventofcode/y2024/Day5Test.java
@@ -0,0 +1,59 @@
+package org.theoriok.adventofcode.y2024;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Day5Test {
+
+    private static final List<String> LIST = List.of(
+        "47|53",
+        "97|13",
+        "97|61",
+        "97|47",
+        "75|29",
+        "61|13",
+        "75|53",
+        "29|13",
+        "97|29",
+        "53|29",
+        "61|53",
+        "97|53",
+        "61|29",
+        "47|13",
+        "75|47",
+        "97|75",
+        "47|61",
+        "75|61",
+        "47|29",
+        "75|13",
+        "53|13",
+        "",
+        "75,47,61,53,29",
+        "97,61,53,29,13",
+        "75,29,13",
+        "75,97,47,61,53",
+        "61,13,29",
+        "97,13,75,29,47"
+    );
+
+    @Test
+    void firstMethod() {
+        var day = new Day5(LIST);
+
+        var result = day.firstMethod();
+
+        assertThat(result).isEqualTo(143);
+    }
+
+    @Test
+    void secondMethod() {
+        var day = new Day5(LIST);
+
+        var result = day.secondMethod();
+
+        assertThat(result).isEqualTo(123);
+    }
+}
