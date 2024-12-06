ff27da0b8590cd7aa7858fdc5d01e49a2a3f2e46
theoriok <kevin.van.dyck@gmail.com>
2024-12-06 13:31:40 +0100

    2024 day 6 part 1

---
+++ src/test/java/org/theoriok/adventofcode/y2024/Day6Test.java
@@ -0,0 +1,41 @@
+package org.theoriok.adventofcode.y2024;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Day6Test {
+
+    private static final List<String> LIST = List.of(
+        "....#.....",
+        ".........#",
+        "..........",
+        "..#.......",
+        ".......#..",
+        "..........",
+        ".#..^.....",
+        "........#.",
+        "#.........",
+        "......#..."
+    );
+
+    @Test
+    void firstMethod() {
+        var day = new Day6(LIST);
+
+        var result = day.firstMethod();
+
+        assertThat(result).isEqualTo(41);
+    }
+
+    @Test
+    void secondMethod() {
+        var day = new Day6(LIST);
+
+        var result = day.secondMethod();
+
+        assertThat(result).isEqualTo(0);
+    }
+}
